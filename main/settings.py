29110c9bd63ad3a2a840b6b7b7b356ec6376e040
adjob1 <aornella@gabs.cl>
2019-11-11 16:23:16 -0300

    default master

--- main/settings.py
+++ main/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -85,7 +86,13 @@ DATABASES = {
         'PORT':3306
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
