0d392d6f46662a531726a82ef3828e72085d05fa
jbaezac <baezakh@gmail.com>
2019-11-22 18:57:33 -0300

    registro clientes

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import views
+from django.contrib.auth.models import User
+from .views import register
+
+class ModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        super(ModelTest,cls).setUpClass()
+        dept = User(username='QA', email='QualityAssurance@gmail.com',password='duoc.2019')
+        dept.save()
+        
+
+    def test_usuario(self):
+        usuario = User.objects.get(username='QA')
+        self.assertEqual(usuario.email,'QualityAssurance@gmail.com')
+
+
+
+'''
+class HomePageTests(TestCase):
+    
+    def test_home_page_status_code(self):
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse(''))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('register'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html') 
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
+'''
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView,PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 from django.contrib.auth.decorators import login_required
 from accounts import views
+from accounts import views as v
 """
 auth/../
 """
 urlpatterns = [
 	path('login/', LoginView.as_view(template_name='login.html'),name="login"),
 	path('logout/', login_required(views.logout_view),name="logout"),
+	path("register/",v.register, name='register'),
 	path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
 	path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
 	path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
